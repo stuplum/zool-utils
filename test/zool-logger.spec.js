3825dde39591f3d4c0eaead9ecfe385f8b9c3b6d
stuplum <stuplum@inkfish.co>
2016-02-12 14:54:16 +0000

    @stuplum Logger can now handle objects as values

--- test/zool-logger.spec.js
+++ test/zool-logger.spec.js
@@ -16,7 +16,7 @@ describe('zool-logger', function () {
         console.warn.restore();
     });
 
-    it('log with colors', function () {
+    it('log [String] with colors', function () {
 
         // Given:
             const logger = zoolLogger('some-label');
@@ -25,7 +25,20 @@ describe('zool-logger', function () {
             logger.log('some-key', 'some-val');
 
         // Then:
-            expect(console.log).to.have.been.calledWith(' some-label: ', '\u001b[90msome-key: \u001b[39m', '\u001b[36msome-val\u001b[39m');
+            expect(console.log).to.have.been.calledWith('\u001b[37m[some-label]\u001b[39m', '\u001b[90msome-key: \u001b[39m', '\u001b[36msome-val\u001b[39m');
+
+    });
+
+    it('log [Object] with colors', function () {
+
+        // Given:
+            const logger = zoolLogger('some-label');
+
+        // When:
+            logger.log('some-key', { some: 'val' });
+
+        // Then:
+            expect(console.log).to.have.been.calledWith('\u001b[37m[some-label]\u001b[39m', '\u001b[90msome-key: \u001b[39m', '\u001b[36m{\n\tsome: \'val\'\n}\u001b[39m');
 
     });
 
@@ -38,7 +51,7 @@ describe('zool-logger', function () {
             logger.debug('some-debug-key', 'some-debug-val');
 
         // Then:
-            expect(console.log).to.have.been.calledWith('\u001b[34m some-debug-label: \u001b[39m', '\u001b[90msome-debug-key: \u001b[39m', '\u001b[36msome-debug-val\u001b[39m' );
+            expect(console.log).to.have.been.calledWith('\u001b[34m[some-debug-label]\u001b[39m', '\u001b[90msome-debug-key: \u001b[39m', '\u001b[36msome-debug-val\u001b[39m');
 
     });
 
@@ -51,8 +64,7 @@ describe('zool-logger', function () {
             logger.error('some-error-key', 'some-error-val');
 
         // Then:
-            expect(console.error).to.have.been.calledWith('\u001b[31m some-error-label: \u001b[39m', '\u001b[90msome-error-key: \u001b[39m', '\u001b[36msome-error-val\u001b[39m');
-
+            expect(console.log).to.have.been.calledWith('\u001b[31m[some-error-label]\u001b[39m', '\u001b[90msome-error-key: \u001b[39m', '\u001b[36msome-error-val\u001b[39m');
     });
 
     it('log warn message with colors', function () {
@@ -64,7 +76,7 @@ describe('zool-logger', function () {
             logger.warn('some-warn-key', 'some-warn-val');
 
         // Then:
-            expect(console.warn).to.have.been.calledWith('\u001b[33m some-warn-label: \u001b[39m', '\u001b[90msome-warn-key: \u001b[39m', '\u001b[36msome-warn-val\u001b[39m');
+            expect(console.log).to.have.been.calledWith('\u001b[33m[some-warn-label]\u001b[39m', '\u001b[90msome-warn-key: \u001b[39m', '\u001b[36msome-warn-val\u001b[39m');
 
     });
 
