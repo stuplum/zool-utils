1eb41f8588b27c1d4ce2640c6177bf484ed724a6
stuplum <stuplum@inkfish.co>
2017-12-14 17:59:17 +0000

    Exports file age service

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+    fileAge: require('./lib/file-age.service'),
     onBoom: require('./lib/on-boom/on-boom'),
     ZoolLogger: require('./lib/zool-logger/zool-logger')
 };
