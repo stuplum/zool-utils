9c2ab863b236a1493a18161f9ae0a887912aade8
stuplum <stuplum@inkfish.co>
2017-12-14 18:12:59 +0000

    Exports zool errors

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-'use strict';
+'use strict'
 
 module.exports = {
-    fileAge: require('./lib/file-age.service'),
-    onBoom: require('./lib/on-boom/on-boom'),
-    ZoolLogger: require('./lib/zool-logger/zool-logger')
-};
+  errors: {
+    CompilationError: require('./lib/errors/compilation.error'),
+    DestNotFoundError: require('./lib/errors/dest-not-found.error'),
+    SrcNotFoundError: require('./lib/errors/src-not-found.error')
+  },
+  fileAge: require('./lib/file-age.service'),
+  onBoom: require('./lib/on-boom/on-boom'),
+  ZoolLogger: require('./lib/zool-logger/zool-logger')
+}
